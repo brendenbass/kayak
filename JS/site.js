335802b3a09d3d3a96bb58346b8b8f2ac2a62e85
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-07-27 17:00:07 -0400

    Planned JavaScript

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,19 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello World");
+//get the user input
+function getValue(){
+
+    //remove spaces and non-letters
+    
+    //lowercase everything
+
+}
+
+//check for palindrome
+function checkValue(value){
+    
+    //create an array with user input inside
+
+    //reverse the array
+
+    //check to see if first user input string is equal to reversed string (should be a truthy statement); if yes, good message; if falsy, bad message
+
 }
