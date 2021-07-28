1bdbde43c4437a0341f1dd2cf06a19df21a1b290
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-07-28 11:18:11 -0400

    Finished Javascript

--- JS/site.js
+++ JS/site.js
@@ -3,30 +3,35 @@ function getValue(){
 
     document.getElementById("alert").classList.add("invisible");
 
-    let userString = document.getElementById("userInput").value;
+    let originalString = document.getElementById("userInput").value.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+
+    let userString = document.getElementById("userInput").value.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
 
     let revString = reverseString(userString);
 
-    displayString(revString);
-    removeAttributes(userString);
+    checkValues(revString, originalString);
 }
 
-function removeAttributes(userString){
+function reverseString(userString){
 
-    //remove spaces and non-letters
-    userString.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+    let revString = [];
 
-    return userString;
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    return revString;
 }
-
-
 //check for palindrome
-function checkValue(value){
-    
-    //create an array with user input inside
-
-    //reverse the array
+function checkValues(revString, originalString){
 
     //check to see if first user input string is equal to reversed string (should be a truthy statement); if yes, good message; if falsy, bad message
-
+    if (revString === originalString) {
+        document.getElementById("alertHeading").innerHTML = `Well done! You entered a palindrome!`;
+        document.getElementById("msg").innerHTML = `Your message reversed is: ${revString}`;
+        document.getElementById("alert").classList.remove("invisible");
+    } else {
+        document.getElementById("alertHeading").innerHTML = `Sorry!`;
+        document.getElementById("msg").innerHTML = `That's not a palindrome.`;
+        document.getElementById("alert").classList.remove("invisible");
+    }
 }
